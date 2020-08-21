41bba69b5b2d33dc167e26a93ae3f44556595606
Basia <b.kumor@yahoo.com>
2020-08-21 21:39:05 +0200

    flipping cards

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const board = document.getElementById('board');
 
-const createBlanks = (arr) => {
+const createCards = (arr) => {
+    let takenId = [];
     let i = 0;
     while (i < arr.length * 2) {
-        let card = document.createElement('img');
-        card.setAttribute('src', './assets/back.png');
-        card.classList.add('back');
-        card.dataset.id = i;
-        board.appendChild(card);
-        card.addEventListener('click', flip);
+        let getId = random(0,5);
+        takenId.push(random);
+        console.log(getId);
+        let card = `<div class="flip-box"><div class="card">
+            <div class="card-back"><img data-id=${i} src="./assets/back.png" id="backface"></div>
+            <div class="card-front"><img data-id=${i} src=${cards[getId].img}></div>
+            </div></div>`;
+        board.insertAdjacentHTML('beforeend', card);
         i++;
     }
+    board.addEventListener('click', flip);
 }
 
-const flip = () => console.log('flipped');
+const flip = e => {
+    if (e.target && e.target.id === 'backface') {
+        e.target.parentElement.parentElement.classList.add('flip-card');
+    }
+};
+
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 
-createBlanks(cards);
+createCards(cards);
